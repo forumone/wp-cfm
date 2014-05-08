e3f0746ff2ddc80ca476338def4dbb0ceb9c0c8e
mgibbs189 <mgibbs189@gmail.com>
2014-05-08 14:21:02 -0400

    Initial commit

---
+++ templates/page-settings.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+    <h2>WP Configuration Management</h2>
+    <p>Placeholder settings page.</p>
+</div>
---
+++ wp-cfm.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Plugin Name: WP-CFM
+Plugin URI: http://forumone.com/
+Description: WordPress Configuration Management
+Version: 1.0.0
+Author: Forum One Communications
+Author URI: http://forumone.com/
+
+Copyright 2014 Forum One Communications
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+defined( 'ABSPATH' ) or die();
+
+class WP_CFM
+{
+
+    function __construct() {
+
+        // setup variables
+        define( 'WP_CFM_VERSION', '1.0.0' );
+        define( 'WP_CFM_DIR', dirname( __FILE__ ) );
+        define( 'WP_CFM_URL', plugins_url( 'wp-cfm' ) );
+
+        // WP is loaded
+        add_action( 'init', array( $this, 'init' ) );
+    }
+
+
+    /**
+     * Initialize classes and WP hooks
+     */
+    function init() {
+
+        // hooks
+        add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+    }
+
+
+    /**
+     * Register the FacetWP settings page
+     */
+    function admin_menu() {
+        add_options_page( 'WP-CFM', 'WP-CFM', 'manage_options', 'wp-cfm', array( $this, 'settings_page' ) );
+    }
+
+
+    /**
+     * Route to the correct edit screen
+     */
+    function settings_page() {
+        include( WP_CFM_DIR . '/templates/page-settings.php' );
+    }
+}
+
+$wp_cfm = new WP_CFM();
