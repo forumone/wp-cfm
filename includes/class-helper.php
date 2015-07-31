c527770b0eed4482c2a0d3ce4cfd49ad9cfff1c5
Andrew Morton <amorton@forumone.com>
2015-07-31 13:10:27 -0700

    #40 Check for valid file in get_source_file_bundles

--- includes/class-helper.php
+++ includes/class-helper.php
@@ -127,13 +127,15 @@ class WPCFM_Helper
         $source_bundles = $this->get_bundle_sources();
         foreach ($source_bundles as $bundle_name => $bundle_source) {
             $bundle_data = WPCFM()->readwrite->read_file( $bundle_name );
-            $bundle_label = $bundle_data['.label'];
-            unset( $bundle_data['.label'] );
-            $output[ $bundle_name ] = array(
-                'label'     => $bundle_label,
-                'name'      => $bundle_name,
-                'config'    => $bundle_data,
-            );
+            if ( !empty( $bundle_data ) ) {
+                $bundle_label = $bundle_data['.label'];
+                unset( $bundle_data['.label'] );
+                $output[ $bundle_name ] = array(
+                    'label'     => $bundle_label,
+                    'name'      => $bundle_name,
+                    'config'    => $bundle_data,
+                );
+            }
         }
         return $output;
     }
