e5227ebda75168d5ec55d2cb2fd1173f678bd9b0
Mickaël Perrin <dev@mickaelperrin.fr>
2017-08-27 22:28:24 +0200

    [FEATURE] Hide already registered options filter

--- wp-cfm.php
+++ wp-cfm.php
@@ -46,6 +46,7 @@ class WPCFM_Core
         define( 'WPCFM_CONFIG_DIR', apply_filters( 'wpcfm_config_dir', WP_CONTENT_DIR . '/config' ) );
         define( 'WPCFM_CONFIG_URL', apply_filters( 'wpcfm_config_url', WP_CONTENT_URL . '/config' ) );
         define( 'WPCFM_CONFIG_FORMAT',  apply_filters( 'wpcfm_config_format', 'json'));
+        define( 'WPCFM_HIDE_ALREADY_REGISTERED', apply_filters( 'wpcfm_config_hide_already_registered', false));
         define( 'WPCFM_URL', plugins_url( '', __FILE__ ) );
 
         // WP is loaded
