59c0e4ed9e8c38f5146d823ae569e6aac7ca2c9b
Mickaël PERRIN <dev@mickaelperrin.fr>
2017-09-10 17:38:56 +0200

    [FEATURE] Test if configuration file exist before import

--- includes/class-wp-cli.php
+++ includes/class-wp-cli.php
@@ -51,10 +51,17 @@ class WPCFM_CLI_Command extends WP_CLI_Command
             WPCFM()->options->is_network = true;
         }
 
-        WPCFM()->readwrite->pull_bundle( $args[0] );
+        $bundle_name = $args[0]?:'all';
+
+        if ($bundle_name != 'all') {
+            if (!in_array($bundle_name, WPCFM()->helper->get_bundle_names())) {
+                WP_CLI::error('Bundle file for "' . $bundle_name . '" can\'t be found');
+            }
+        }
+
+        WPCFM()->readwrite->pull_bundle($bundle_name);
         WP_CLI::success( 'The bundle has been pulled into the database.' );
     }
-
 
     /**
      * Compare bundle differences
